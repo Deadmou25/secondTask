0d95f9fd7519b865f938ef8b95dcdcd3c18824e9
Deadmou25 <56234754+Deadmou25@users.noreply.github.com>
2023-04-11 20:33:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secondTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 22;
+            string name = "Andrey";
+            double price = 2.5;
+            Object anyValue = 5.5;
+            short shortNumber = 12;
+            long longNumber = 121333243242;
+            decimal veryLargNumber = 994783294732947;
+            char firstLetterAlphabet = 'A';
+            float pi = 3.14f;
+            byte numberFive = 101;
+        }
+    }
+}
